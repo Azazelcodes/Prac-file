d92afa4c074bc5198bb0968ed8a498fdc8b7c1f3
Shlok/ Azazel <84954530+Azazelcodes@users.noreply.github.com>
2021-09-07 21:24:15 +0530

    Added Q1

---
+++ Q1.py
@@ -0,0 +1,7 @@
+x=int(input("enter first number:"))
+y=int(input("enter second number:"))
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+x, y = y, x
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
